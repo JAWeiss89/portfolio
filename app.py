b4dde5ee136bcc8d3c9ddcb218ae13ea6d033cc5
Jorge Weiss <jorge.a.weiss@gmail.com>
2020-08-19 01:50:59 -0500

    Make page responsive

--- app.py
+++ app.py
@@ -8,7 +8,3 @@ def show_home_page():
     """ Shows home page """
     return render_template('index.html')
 
-@app.route('/resume')
-def show_resume():
-    """ Gets resume PDF """
-    return render_template('Weiss_Resume.pdf')
